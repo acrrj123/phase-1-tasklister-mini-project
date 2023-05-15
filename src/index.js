a2de20b4ff041fcdf569f6c5c3701bd34b983f13
Anna Cole <acrrj123@gmail.com>
2023-05-15 13:58:12 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    buildToDo(e.target['new-task-description'].value)
+    form.reset()
+  })
 });
+
+function buildToDo(todo) {
+  let list = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  list.textContent = `${todo} `
+  list.appendChild(btn)
+  document.querySelector('#tasks').appendChild(list)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
+
+
+
